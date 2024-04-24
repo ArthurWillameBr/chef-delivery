6e333dc3d7e04f6a9abae042fd7766ac41fb2db3
arthurwillame5 <135058422+arthurwillame5@users.noreply.github.com>
2024-04-24 17:53:31 -0300

    chore: add base layout

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "./_components/ui/button";
+
+const Home = () => {
+  return (
+    <div className="mx-6 h-screen w-full bg-red-500 py-5">
+      <Button>Clicar</Button>
+    </div>
+  );
+};
+
+export default Home;
