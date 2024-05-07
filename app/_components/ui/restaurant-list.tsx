8f0d7de10fb01099809dbcc0bccb8727389f4f7f
arthurwillame5 <135058422+arthurwillame5@users.noreply.github.com>
2024-05-07 14:11:56 -0300

    fix: scroll bar to restaurant list

--- app/_components/ui/restaurant-list.tsx
+++ app/_components/ui/restaurant-list.tsx
@@ -5,7 +5,7 @@ import { db } from "@/app/_lib/prisma";
 const RestaurantList = async() => {
     const restaurants = await db.restaurant.findMany({take: 10});
     return ( 
-        <div className="flex gap-4 px-5 overflow-x-hidden ">
+        <div className="flex gap-4 overflow-x-scroll px-5 [&::-webkit-scrollbar]:hidden">
             {restaurants.map((restaurant) => (
                 <RestaurantItem key={restaurant.id} restaurant={restaurant} />
             ))}
